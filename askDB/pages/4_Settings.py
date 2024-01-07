0dbc9ae2e812511fd85c9065c89d2eedfdd92036
styx0r <christoph.baldow@gmail.com>
2024-01-06 21:16:08 -0500

    adds Settings page as reminder

---
+++ askDB/pages/4_Settings.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.sidebar.markdown(
+    f"""
+Here you can configure:
+- open ai key
+- database connections
+
+Note: First we need a user authentication and a database where we can store this information
+    """
+)
+
+st.markdown("# OpenAI Key")
+st.markdown("ToDo")
+st.markdown("# Database Connection")
+st.markdown("ToDo")
