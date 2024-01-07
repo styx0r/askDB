e4dc49af521ef982dcd56ccf14e90f360526654a
styx0r <christoph.baldow@gmail.com>
2024-01-06 21:13:19 -0500

    adds warning if openAI api key is missing

--- askDB/Insides.py
+++ askDB/Insides.py
@@ -7,6 +7,10 @@ import os
 import streamlit as st
 
 st.session_state["env"] = os.environ.get("ENV", "dev")
+if "OPENAI_API_KEY" not in os.environ:
+    st.markdown(
+        "Please set the OPENAI_API_KEY environment variable in order to successfully start the app."
+    )
 
 st.sidebar.markdown("### What next?")
 st.sidebar.markdown("Type in a query in natural language against your database.")
